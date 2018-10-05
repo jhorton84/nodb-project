f9010f7e1adce26146ae243b89113c518b9a594c
Justin Horton <justinhorton@Justins-MacBook-Pro.local>
2018-10-05 11:26:56 -0700

    initial commit

---
+++ server/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.json());
+const data = "You will become an amazing web developer"
+
+app.get('/data', (req, res) => {
+    res.status(200).send(data);
+})
+
+const PORT = 4000;
+app.listen(PORT, () => console.log(`Server has sailed from port ${PORT} ⛵️`));
--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      fortune: 'Awaiting your orders...'
+    }
+  }
+  fetchData() {
+    axios.get('/data').then(res => {
+      this.setState({
+        fortune: res.data
+      })
+    })
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>Tell me my fortune</h1>
+        <button onClick = {() => this.fetchData()}>Fetch Fortune</button>
+        <p>{this.state.fortune}</p>
       </div>
     );
   }
